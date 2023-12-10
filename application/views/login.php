522d5f78365e699c1839ab8e7a46865d57c87061
kamalakbar <muhamadkamal275@gmail.com>
2023-12-10 08:44:15 +0700

    done projek-app

--- application/views/login.php
+++ application/views/login.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>SB Admin 2 - Login</title>
+	<title>Toko Outdoor Ilal - Login</title>
 	<link href="<?= base_url('sb-admin') ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	<link href="<?= base_url('sb-admin') ?>/css/sb-admin-2.min.css" rel="stylesheet">
@@ -48,7 +48,7 @@
 										</div>
 									<?php endif ?>
 									<div class="text-center">
-										<h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+										<h1 class="h4 text-gray-900 mb-4">Aplikasi RECAD<br>Toko Outdoor Ilal</h1>
 									</div>
 									<form class="user" method="POST" action="<?= base_url('login/proses_login') ?>">
 										<div class="form-group">
---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="" target="_blank">IlalOutdoor</a></span>
+		</div>
+	</div>
+</footer>
